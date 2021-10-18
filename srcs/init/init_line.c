90bef3fde11e8fae9bbe2f3de7854f5e16c5d120
hb-kwon <gurqo1223@gmail.com>
2021-10-18 19:47:53 +0900

    Minishell todo valid check after parsing cmd & token

--- srcs/init/init_line.c
+++ srcs/init/init_line.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 14:02:52 by hkwon             #+#    #+#             */
-/*   Updated: 2021/10/18 19:38:46 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/10/18 19:44:55 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ void	init_line(t_mini *shell)
 	char	buf[PATH_MAX];
 
 	//전역변수
-	g_mini.path = ft_strjoin(getcwd(buf, PATH_MAX), " > ");
+	g_mini.path = ft_strjoin(getcwd(buf, PATH_MAX), "$ ");
 	g_mini.line = readline(g_mini.path);
 	if (g_mini.line == NULL)
 	{
-		//커서를 한 줄 위로, 12칸 뒤로 이동하는 기능
-		// ft_putstr_fd("\x1b[1A", STDOUT);
-		// ft_putstr_fd("\033[12C", STDOUT);
+		//커서를 한 줄 위로, len만큼 뒤로 이동하는 기능
 		printf("\x1b[1A\033[%zuCexit\n", ft_strlen(g_mini.path));
 		free(g_mini.path);
 		free(g_mini.line);
--- srcs/parsing/parse_check.c
+++ srcs/parsing/parse_check.c
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_check.c                                       :+:      :+:    :+:   */
+/*   parse_check.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/14 16:03:49 by hkwon             #+#    #+#             */
-/*   Updated: 2021/10/18 19:47:21 by hkwon            ###   ########.fr       */
+/*   Created: 2021/10/18 19:45:33 by hkwon             #+#    #+#             */
+/*   Updated: 2021/10/18 19:47:14 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// int	init_check(char *line)
-// {
-// 	int	i;
-// 	int	f;
-// 	int	rd;
-
-// 	i = 0;
-// 	if (line == NULL)
-// 		return (0);
-// 	while (line[i])
-// 	{
-// 		//flag	redirect/pipe/quote
-// 	}
-// 	return (1);
-// }
+void	parse_check(t_cmd *cmd)
+{
+	//line check->cmd * token check
+	//todo valid check after parsing cmd & token
+}
