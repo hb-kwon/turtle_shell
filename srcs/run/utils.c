41e3edc328403a99a6f85c85db06334c74a564b6
hb-kwon <gurqo1223@gmail.com>
2021-10-25 12:46:12 +0900

    Minishell no quote string -ing

--- srcs/run/utils.c
+++ srcs/run/utils.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 14:30:16 by ysong             #+#    #+#             */
-/*   Updated: 2021/10/20 21:09:07 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/10/25 12:45:35 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,50 @@ int print_error2(char *msg1, char *msg2, char *err_num)
 	return (-1);
 }
 
+// char	*ft_str_char_join(char *before, char c)
+// {
+// 	int		len;
+// 	char	*after;
+// 	int		i;
+
+// 	len = ft_strlen(before);
+// 	if (c == '\n')
+// 		return (before);
+// 	else
+// 	{
+// 		after = (char *)malloc(sizeof(char) * (len + 2));
+// 		i = -1;
+// 		if (before == NULL)
+// 			after[0] = c;
+// 		else
+// 		{
+// 			while (before[++i])
+// 				after[i] = before[i];
+// 			after[i] = c;
+// 			free(before);
+// 		}
+// 		after[len + 1] = '\0';
+// 	}
+// 	return (after);
+// }
+
+char	*check_quote(char *buff)
+{
+	int	i;
+	int	j;
+	char	*res;
+
+	i = -1;
+	j = 0;
+	while (buff[++i])
+	{
+		if (buff[i] == 34)
+			i++;
+		printf("buff : %c\n", buff[i]);
+	}
+	return (buff);
+}
+
 char	**make_buff(t_mini *shell)
 {
 	t_token	*temp;
@@ -50,13 +94,15 @@ char	**make_buff(t_mini *shell)
 		j++;
 	}
 	buff = (char **)malloc(sizeof(char *) * (j + 1));
+	buff[j] = NULL;
 	while (temp)
 	{
 		buff[i] = temp->arg;
+		buff[i] = check_quote(buff[i]);
+		printf("check buff after check_quote : %s\n", buff[i]);
 		temp = temp->next;
 		i++;
 	}
-	buff[j] = NULL;
 	return (buff);
 }
 
