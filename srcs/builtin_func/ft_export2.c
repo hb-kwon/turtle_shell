c13bbae6230b44173d287bef8f9c1a5ad86ee871
djgnfj-svg <djgnfj8923@naver.com>
2021-11-11 22:06:48 +0900

    Add error case export2

--- srcs/builtin_func/ft_export2.c
+++ srcs/builtin_func/ft_export2.c
@@ -6,7 +6,7 @@
 /*   By: ysong <ysong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 02:17:30 by ysong             #+#    #+#             */
-/*   Updated: 2021/11/11 02:37:42 by ysong            ###   ########.fr       */
+/*   Updated: 2021/11/11 22:06:20 by ysong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ int	is_export_valid(char *str)
 
 	i = -1;
 	if (ft_isdigit(str[++i]))
-	{
 		return (0);
-	}
-	//todo 나중에 더많은 예외처리 추가예정
+	if (str[0] == '$' && str[1])
+		return (1);
+	if (str[0] == '$')
+		return (0);
 	return (1);
 }
