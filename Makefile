b7c5a5901a572fb828c5d67694d76cb16f2ed5db
hb-kwon <gurqo1223@gmail.com>
2021-11-15 17:58:55 +0900

    Minishell signal test

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: ysong <ysong@student.42.fr>                +#+  +:+       +#+         #
+#    By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/09/04 03:12:03 by kwonhyukbae       #+#    #+#              #
-#    Updated: 2021/11/13 13:56:58 by ysong            ###   ########.fr        #
+#    Updated: 2021/11/15 17:58:25 by hkwon            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,8 +14,8 @@ NAME = minishell
 LIBFT = libft.a
 
 CC = gcc
-# CFLAGS = -Wall -Wextra -Werror
-CFLAGS = -g3 -fsanitize=address
+CFLAGS = -Wall -Wextra -Werror
+#CFLAGS = -g3 -fsanitize=address
 # CFLAGS = -g
 AR = ar rc
 RM = rm -f
--- srcs/init/init_shell.c
+++ srcs/init/init_shell.c
@@ -6,7 +6,7 @@
 /*   By: hkwon <hkwon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 16:45:14 by hkwon             #+#    #+#             */
-/*   Updated: 2021/11/15 17:48:11 by hkwon            ###   ########.fr       */
+/*   Updated: 2021/11/15 17:58:17 by hkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,67 +34,49 @@ void	init_env(char ***en, char *envp[])
 	return ;
 }
 
+static void	signal_int_util()
+{
+	if (rl_on_new_line() == -1)
+		exit(1);
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
 static void	signal_int(int signo)
 {
-	// if (g_mini.pid > 0)
-	// {
-	// 	if (!kill(g_mini.pid, signo))
-	// 	{
-	// 		printf("\033[%luC  \b\b\n", ft_strlen(g_mini.path));
-	// 		g_mini.sig_on = 1;
-	// 	}
-	// 	else
-	// 	{
-	// 		printf("\033[%luC  \b\b\n", ft_strlen(g_mini.path));
-	// 		if (rl_on_new_line() == -1)
-	// 			exit(1);
-	// 		rl_replace_line("", 0);
-	// 		rl_redisplay();
-	// 	}
-	// }
-	// else
-	// {
-	// 	printf("\033[%luC  \b\b\n", ft_strlen(g_mini.path));
-	// 	if (rl_on_new_line() == -1)
-	// 		exit(1);
-	// 	rl_replace_line("", 0);
-	// 	rl_redisplay();
-	// }
 	if (g_mini.pid > 0)
 	{
+		if (!kill(g_mini.pid, signo))
+		{
 			printf("\033[%luC  \b\b\n", ft_strlen(g_mini.path));
 			g_mini.sig_on = 1;
+		}
+		else
+		{
+			printf("\033[%luC  \b\b\n", ft_strlen(g_mini.path));
+			signal_int_util();
+		}
 	}
-	else (g_mini.pid == 0)
+	else
 	{
 		printf("\033[%luC  \b\b\n", ft_strlen(g_mini.path));
-		if (rl_on_new_line() == -1)
-			exit(1);
-		rl_replace_line("", 0);
-		rl_redisplay();
+		signal_int_util();
 	}
 }
 
 static void	signal_quit(int signo)
 {
-	// if (g_mini.pid > 0)
-	// {
-	// 	if (!kill(g_mini.pid, signo))
-	// 	{
-	// 		printf("\033[%luC  \b\bQuit: 3\n", ft_strlen(g_mini.path));
-	// 		g_mini.sig_on = 1;
-	// 	}
-	// 	else
-	// 		printf("\033[%luC  \b\b", ft_strlen(g_mini.path));
-	// }
-	// else
-	// 	printf("\033[%luC  \b\b", ft_strlen(g_mini.path));
 	if (g_mini.pid > 0)
 	{
+		if (!kill(g_mini.pid, signo))
+		{
 			printf("\033[%luC  \b\bQuit: 3\n", ft_strlen(g_mini.path));
 			g_mini.sig_on = 1;
+		}
+		else
+			printf("\033[%luC  \b\b", ft_strlen(g_mini.path));
 	}
-	else if (g_mini.pid == 0)
+	else
 		printf("\033[%luC  \b\b", ft_strlen(g_mini.path));
 }
 
